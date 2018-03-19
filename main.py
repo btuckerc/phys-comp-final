72d1752c8d982e4f75d3a70eb0d1b1370d962117
Tucker <35579682+tucraig@users.noreply.github.com>
2018-03-19 02:08:14 -0400

    Update main

--- main.py
+++ main.py
@@ -2,15 +2,28 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+body_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+
 cap = cv2.VideoCapture(1)
-fgbg = cv2.createBackgroundSubtractorMOG2()
 
 while(True):
     ret, frame = cap.read()
-    fgmask = fgbg.apply(frame)
 
-    cv2.imshow('fgmask',frame)
-    cv2.imshow('frame',fgmask)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+    	cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+    	#roi_gray = gray[y:y+h, x:x+w] #region of image
+    	#roi_color = frame[y:y+h, x:x+w]
+    	#eyes = ...(roi_gray)
+
+    bodies = body_cascade.detectMultiScale(gray)
+    for (x,y,w,h) in bodies:
+    	cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 2)
+
+    cv2.imshow('frame',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
