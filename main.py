582ed33eb16b98a3e7ef1ce24760e9ae54208b9c
Tucker <35579682+tucraig@users.noreply.github.com>
2018-03-27 19:54:21 -0400

    Update main add bicycle xml (broken)

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from matplotlib import pyplot as plt
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 body_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+bike_cascade = cv2.CascadeClassifier('bicycle.xml')
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    bodies = body_cascade.detectMultiScale(gray, 1.3, 5)
+    bikes = bike_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
     	cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
@@ -19,15 +22,18 @@ while(True):
     	#roi_color = frame[y:y+h, x:x+w]
     	#eyes = ...(roi_gray)
 
-    bodies = body_cascade.detectMultiScale(gray)
-    for (x,y,w,h) in bodies:
-    	cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 2)
+    # bodies = body_cascade.detectMultiScale(gray)
+    # for (x,y,w,h) in bodies:
+    # 	cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 2)
+
+    for (x,y,w,h) in bikes:
+    	cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
 
     cv2.imshow('frame',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
+
 
 cap.release()
 cv2.destroyAllWindows()
