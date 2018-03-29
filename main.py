bc1f8f320cce50b0449b25a0978258bc00b17891
Tucker <35579682+tucraig@users.noreply.github.com>
2018-03-29 16:48:09 -0400

    Add avg_color

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ body_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 uBody_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
 #bike_cascade = cv2.CascadeClassifier('bicycle.xml')
 
+body_colors = []
+face_colors = []
+
+avg_body = np.zeros((300, 300, 3), np.uint8)
+avg_face = np.zeros((300, 300, 3), np.uint8)
+
 cap = cv2.VideoCapture(0)
 
 try:
@@ -22,23 +28,29 @@ try:
         for i,(x,y,w,h) in enumerate(faces):
             cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2) #BGR
             face = frame[y:y+h, x:x+w]
+            face_avg_color_per_row = np.average(face, axis=0)
+            face_avg_color = np.average(face_avg_color_per_row, axis=0)
+            face_colors.append(face_avg_color)
             #roi_gray = gray[y:y+h, x:x+w] #region of image
         	#roi_color = frame[y:y+h, x:x+w]
         	#eyes = ...(roi_gray)
 
-            print(avg_color)
-
-        uBodies = body_cascade.detectMultiScale(gray)
         for (x,y,w,h) in uBodies:
             cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 2)
             body = frame[y:y+h, x:x+w]
             body_avg_color_per_row = np.average(body, axis=0)
-            body_avg_color = np.average(avg_color_per_row, axis=0)
+            body_avg_color = np.average(body_avg_color_per_row, axis=0)
+            body_colors.append(body_avg_color)
 
         #for (x,y,w,h) in bikes:
         #	cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
 
         cv2.imshow('frame',frame)
+
+        avg_body[:] = np.average(body_colors,axis=0)
+        avg_face[:] = np.average(face_colors,axis=0)
+        cv2.imwrite('avg_body.jpg',avg_body)
+        cv2.imwrite('avg_face.jpg',avg_face)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
